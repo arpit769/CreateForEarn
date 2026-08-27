952a91be90aa5f35e282d93df2eae92213909d1b
Saharsh Singh <saharshsingh171105@gmail.com>
2026-08-27 22:16:50 +0530

    add video tasks

--- add_enum_values.sql
+++ add_enum_values.sql
@@ -1,5 +1,6 @@
 -- Add missing values to content_mode_enum
 ALTER TYPE content_mode_enum ADD VALUE IF NOT EXISTS 'image';
+ALTER TYPE content_mode_enum ADD VALUE IF NOT EXISTS 'video';
 ALTER TYPE content_mode_enum ADD VALUE IF NOT EXISTS 'text';
 ALTER TYPE content_mode_enum ADD VALUE IF NOT EXISTS 'provided';
 ALTER TYPE content_mode_enum ADD VALUE IF NOT EXISTS 'custom';
